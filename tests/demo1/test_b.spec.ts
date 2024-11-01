350b514a3d8977557bee97389ae70e36077f8fb2
m <mihaidmuresan@gmail.com>
2024-11-01 13:58:54 +0200

    added demo tests

---
+++ tests/demo1/test_b.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test_b-has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('test_b-get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
